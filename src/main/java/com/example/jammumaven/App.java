49ec96b66ff30db107c4ba905a08f16c57f5547a
rahulkum436424 <kumrahul436@gmail.com>
2025-05-06 12:23:43 +0530

    Initial commit

---
+++ src/main/java/com/example/jammumaven/App.java
@@ -0,0 +1,10 @@
+package com.example.jammumaven;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
